2d85acc2a6995834b645c677a5bc0d60d999e895
tkozuch <kozuch.tomasz1@gmail.com>
2024-04-16 16:51:12 +0200

    assembly parsing

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+
+import { getRequestWithNativeFetch } from "./utilities";
+
+function App() {
+  /**
+   * @type {{data: {userId: Number, id: Number, title: String, body: String}[]}}
+   */
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    getRequestWithNativeFetch
+  );
+  const [wordCount, setWordCount] = useState(new Map());
+
+  async function calculateWords(data) {
+    let pyodide = await window.loadPyodide();
+    const locals = pyodide.toPy({ posts: data, word: "et" });
+    let countWords = pyodide.runPython(`
+        def countWords(data):
+            print(data["word"], data["posts"])
+            return {post["id"]: post["body"].split().count(data["word"]) for post in data["posts"]}
+        countWords
+    `);
+
+    let result = countWords(locals);
+    if (result) {
+      let unpack = result.toJs();
+      setWordCount(unpack);
+    }
+  }
+
+  useEffect(() => {
+    if (data) {
+      console.log("data present");
+      calculateWords(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log("word count changed: ", wordCount);
+  }, [wordCount]);
+
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : data ? (
+        <div>
+          Data:
+          {wordCount && (
+            <div>
+              Result:
+              {[...wordCount.entries()].map(([postId, wordCount]) => {
+                return (
+                  <div key={postId}>
+                    PostID: {postId}, WordCount: {wordCount}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>No data</div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/utilities.js
@@ -0,0 +1,9 @@
+export const getRequestWithNativeFetch = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: Status ${response.status}`);
+  }
+
+  return response.json();
+};
