b5592bb0a52a1bb283874bdc5605c296b71d5431
tkozuch <kozuch.tomasz1@gmail.com>
2024-04-17 13:54:47 +0200

    responsive + desktop

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   }, [data]);
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-neutral-600 text-white">
+    <div className="w-full h-screen flex flex-col justify-center items-center px-8 md:px-12 lg:px-16 max-w-7xl">
       {isLoading ? (
         <div className="text-xl flex flex-col justify-center">Loading...</div>
       ) : error ? (
@@ -51,17 +51,14 @@ function App() {
           data-x-label="Post ID"
           data-y-label={`Word "${wordToCount}" count`}
           className={
-            "max-w-60 lg:max-w-7xl overflow-auto after:content-[attr(data-x-label)] " +
+            "w-full h-1/3 overflow-auto after:content-[attr(data-x-label)] " +
             // additional titles
-            "after:absolute after:left-1/2 after:-translate-x-1/2 before:content-[attr(data-y-label)] before:absolute before:top-1/2 before:-translate-y-1/2 before:-rotate-90 before:-translate-x-1/2 before:pb-12 after:text-white before:text-white"
+            "after:absolute after:left-1/2 after:-translate-x-1/2 before:content-[attr(data-y-label)] before:absolute before:top-1/2 before:-translate-y-1/2 before:-rotate-90 before:-translate-x-1/2 before:pb-8 after:text-white before:text-white" +
+            // accommodate for title
+            " pl-6"
           }
         >
-          <div className="w-60 h-80">
-            <Chart
-              data={wordCount}
-              label={`Word "${wordToCount}" count`}
-            ></Chart>
-          </div>
+          <Chart data={wordCount} label={`Word "${wordToCount}" count`}></Chart>
         </div>
       ) : (
         <div>No data</div>
